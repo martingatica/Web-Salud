f665f1be954b0a61878ab88d4e590a83e25dfc11
Martín Gatica <martingatica@uc.cl>
2024-12-19 20:01:46 -0300

    Agregar funcionalidades admin y pagina admin

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -13,14 +13,17 @@ import { FormFieldType } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-    userId, patientId, type
+    userId, patientId, type, appointment, setOpen
 }: {
     userId: string;
     patientId: string;
     type: "create" | "cancel" | "schedule";
+    appointment: Appointment
+    setOpen: (open: boolean) => void;
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -30,11 +33,11 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment.primaryPhysician : "",
+      schedule: appointment ? new Date(appointment.schedule) : new Date(Date.now()),
+      reason: appointment ? appointment.reason : "",
+      note: appointment ? appointment.note : "",
+      cancellationReason: appointment?.cancellationReason || "",
     },
   })
 
@@ -47,7 +50,7 @@ const AppointmentForm = ({
             status = "scheduled";
             break;
         case "cancel":
-            status = "canceled";
+            status = "cancelled";
             break;
         default:
             status = "pending";
@@ -74,6 +77,24 @@ const AppointmentForm = ({
             form.reset();
             router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
         }
+      } else {
+        const appointmentToUpdate ={
+            userId,
+            appointmentId: appointment?.$id!,
+            appointment: {
+                primaryPhysician: values?.primaryPhysician,
+                schedule: new Date(values?.schedule),
+                status: status as Status,
+                cancellationReason: values?.cancellationReason,
+            },
+            type
+        }
+            const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+            if(updatedAppointment) {
+                setOpen && setOpen(false);
+                form.reset();
+          }
       }
     } catch (error) {
       console.error(error);
@@ -90,7 +111,7 @@ const AppointmentForm = ({
         buttonLabel = "Create Appointment";
         break;
     case "schedule":
-        buttonLabel = "Schedul Appointment";
+        buttonLabel = "Schedule Appointment";
         break;
     default:
         break;
@@ -99,10 +120,10 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-14">
+        {type === "create" && <section className="mb-12 space-y-14">
           <h1 className="header">New Appointment</h1>
           <p className="text-dark-700">Request a new appointment in 10 seconds.</p>
-        </section>
+        </section>}
 
         {type !== "cancel" && (
             <>
